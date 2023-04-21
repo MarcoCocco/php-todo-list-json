b5c67825b7a60a98519d7a475d379907b27ba281
MarcoCocco <marcococco28@gmail.com>
2023-04-21 10:29:28 +0200

    feat: fix

--- js/script.js
+++ js/script.js
@@ -13,29 +13,38 @@ createApp({
 
     methods: {
 
+        //Richiesta API dell'array json
         getToDoList() {
+
             axios.get('./server.php').then(response => {
 
                 this.toDoList = response.data;
-                console.log(response.data)
+
 
             });
         },
 
         addNewToDo() {
-            let newToDoObj = {
-                element: this.toDoItem,
-                isDone: false
+            //Creazione nuovo elemento dell'oggetto
+            let data = {
+                toDoItem: '',
             }
+            
+            data.toDoItem = this.toDoItem;
 
-            this.toDoList.push(newToDoObj);
+            //this.toDoList.push(this.toDoItem);
             this.toDoItem = '';
+
+            axios.post('./server.php', data, { headers: { 'Content-Type': 'multipart/form-data' } }).then(response => {
+                this.getToDoList();
+            });
+           
         },
+
 
     },
 
     mounted() {
         this.getToDoList();
     },
-
 }).mount('#app');
