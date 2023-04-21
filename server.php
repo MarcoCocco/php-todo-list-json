59289614c61d95a21f37a9710fa46a912a897152
MarcoCocco <marcococco28@gmail.com>
2023-04-21 10:29:06 +0200

    feat: aggiunta la chiamata post che salva il nuovo oggetto nell'array del file json

--- server.php
+++ server.php
@@ -1,13 +1,24 @@
-<?php 
+<?php
 
-//Salvo l'array presente nel file json per poterlo utilizzare nell'app
-$fileString = file_get_contents('todos.json');
+if (isset($_POST['toDoItem'])) {
 
-//Lo converto in un array PHP
-$toDoList = json_decode($fileString);
+    $toDoItem = array();
+    $toDoItem['element'] = $_POST['toDoItem'];
+    $toDoItem['isDone'] = false;
+    $toDoJSON = file_get_contents('todos.json');
+    $toDoList = json_decode($toDoJSON);
+    $toDoList[] = $toDoItem;
+    $newToDoJSON = json_encode($toDoList);
+    file_put_contents('todos.json', $newToDoJSON);
+    
+} else {
 
-//Funzione di specifica del tipo di file
-header('Content-type: application/json');
-
-//Stampa dell'array convertito in PHP in formato json
-echo json_encode($toDoList);
+    //Salvo l'array presente nel file json per poterlo utilizzare nell'app
+    $fileString = file_get_contents('todos.json');
+    //Lo converto in un array PHP
+    $toDoList = json_decode($fileString);
+    //Funzione di specifica del tipo di file
+    header('Content-type: application/json');
+    //Stampa dell'array convertito in PHP in formato json
+    echo json_encode($toDoList);
+}
